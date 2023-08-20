eec065b85e296772957de4618750af51430ba24b
Traderishka <jenyuxa1@gmail.com>
2023-08-20 20:44:45 +0300

    lesson25 "extends"

---
+++ lessons/src/lesson25/Animal.java
@@ -0,0 +1,11 @@
+package lesson25;
+
+public class Animal {
+    public void eat() {
+        System.out.println("i am eat");
+    }
+
+    public void slipping() {
+        System.out.println("i'm sleep");
+    }
+}
